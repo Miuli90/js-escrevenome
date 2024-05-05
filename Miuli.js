53e741df55a625c8f2be9ed34cc6389579f13aa0
Miuli90 <168981534+Miuli90@users.noreply.github.com>
2024-05-05 13:34:50 -0300

    Adicionando arquivo js

---
+++ Miuli.js
@@ -0,0 +1,19 @@
+// colaborador: frangof321!
+let x, y, r, g, b;
+
+function setup() {
+createCanvas(windowWidth,
+windowHeight);
+background(0);
+}
+
+function draw() {
+r = random(255);
+g = 0;
+b = random(255);
+X = random(width);
+y = random(height);
+noStroke();
+fill(r, g, b, 100);
+circle(x, y, 24);
+}
